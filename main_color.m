e095b7abc7da3864b44bfa8427e923237f1e3f9e
ZhangYong19800721 <24452861@qq.com>
2017-07-16 00:19:23 +0800

    update

--- main_color.m
+++
@@ -1,8 +0,0 @@
-clear all;
-close all;
-format compact;
-
-SURF = ConstructSurfData_Color('D:\imagebase\','*.jpg');
-VisualVocabulary_UV = ConstructVisualVocabulary(124,SURF,5);
-
-save('VisualVocabulary_UV.mat','VisualVocabulary_UV');
