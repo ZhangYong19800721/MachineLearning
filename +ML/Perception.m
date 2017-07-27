6070fa00cb1903178ec179ab109d8887154b737b
ZhangYong19800721 <24452861@qq.com>
2017-07-27 14:06:06 +0800

    update

--- +ML/Perception.m
+++ +ML/Perception.m
@@ -29,6 +29,7 @@ classdef Perception
         
         function obj = train(obj,minibatchs,learn_rate_min,learn_rate_max,max_it)
             minibatch_num = length(minibatchs);
+            layer_num = length(obj.bias);
             ob_window_size = minibatch_num;
             ob = ML.Observer('均方误差',1,ob_window_size,'xxx');
             
@@ -47,7 +48,7 @@ classdef Perception
                 minibatch_idx = mod(it,minibatch_num) + 1;
                 labels = minibatchs{minibatch_idx}.labels;
                 points = minibatchs{minibatch_idx}.points; 
-                % minibatch_size = size(points,2);
+                minibatch_size = size(points,2);
                 error = sum(sqrt(sum((labels - obj.do(points)).^2,2)));
                 error_list(minibatch_idx) = error;
                 error_ave_new = mean(error_list);
@@ -66,11 +67,23 @@ classdef Perception
                 description = strcat(description,strcat('学习速度:',num2str(learn_rate)));
                 ob = ob.showit(error_ave_new,description);
                 
-                delta = obj.BP(points(:,1),labels(:,1));
-%                 delta_w = 2 * delta * points' / minibatch_size;
-%                 delta_b = 2 * delta * ones(minibatch_size,1) / minibatch_size;
-%                 obj.weight = obj.weight + learn_rate * delta_w;
-%                 obj.bias   = obj.bias   + learn_rate * delta_b;
+                for m = 1:layer_num
+                    delta{m}.weight = zeros(size(obj.weight{m}));
+                    delta{m}.bias   = zeros(size(obj.bias  {m}));
+                end
+                
+                for n = 1:minibatch_size
+                    delta_n = obj.BP(points(:,n),labels(:,n));
+                    for m = 1:layer_num
+                        delta{m}.weight = delta{m}.weight + delta_n{m}.weight;
+                        delta{m}.bias   = delta{m}.bias   + delta_n{m}.bias;
+                    end
+                end
+                
+                for m = 1:layer_num
+                    obj.weight{m} = obj.weight{m} - learn_rate * delta{m}.weight / minibatch_size;
+                    obj.bias{m}   = obj.bias{m}   - learn_rate * delta{m}.bias   / minibatch_size;
+                end
             end
         end
         
@@ -99,26 +112,26 @@ classdef Perception
             M = length(obj.bias); % 得到层数
 
             [~,~,a] = obj.do(point); % 首先执行正向计算,并记录每一层的输出
-            s{M} = diag(2 * (label - a{M}) .* a{M} .* (a{M} - 1)); % 计算顶层的敏感性
+            s{M} = 2 * (label - a{M}) .* a{M} .* (a{M} - 1); % 计算顶层的敏感性
             
             for m = (M-1):-1:1  % 反向传播敏感性
-                s{m} = s{m+1} * obj.weight{m+1} * diag(a{m} .* (1 - a{m})); 
+                s{m} = diag(a{m} .* (1 - a{m})) * obj.weight{m+1}' * s{m+1}; 
             end
             
             for m = 1:M  % 计算梯度
                 if m == 1 
-                    delta{m}.weight = s{m} * repmat(point',size(a{m}));
-                    delta{m}.bias = 1;
+                    delta{m}.weight = repmat(s{m},1,size(point, 1)) .* repmat(point' ,size(a{m},1),1);
+                    delta{m}.bias   = s{m};
                 else
-                    delta{m}.weight = s{m} * repmat(a{m-1}',size(a{m-1},1),1);
-                    delta{m}.bias = 1;
+                    delta{m}.weight = repmat(s{m},1,size(a{m-1},1)) .* repmat(a{m-1}',size(a{m},1),1);
+                    delta{m}.bias   = s{m};
                 end
             end
         end
     end
     
     methods(Static)
-        function [perception,e] = unit_test()
+        function [perception,e] = unit_test1()
             clear all;
             close all;
             [train_images,train_labels,test_images,test_labels] = ML.import_mnist('./+ML/mnist.mat');
@@ -137,9 +150,61 @@ classdef Perception
             perception = perception.train(mnist,1e-6,0.1,1e6);
             
             save('perception.mat','perception');
+        end
+        
+        function [perception,e] = unit_test2()
+            clear all;
+            close all;
+            daata = 100 * rand(2,1e4);
+            label = daata(1,:) > daata(2,:);
+            [D,N] = size(daata); K = 1;
             
-%             y = perception.classify(test_images);
-%             e = sum(y~=test_labels') / length(y);
+            for minibatch_idx = 1:100
+                data{minibatch_idx}.labels = label(:,(100 * (minibatch_idx - 1) + 1):(100 * (minibatch_idx - 1) + 100));
+                data{minibatch_idx}.points = daata(:,(100 * (minibatch_idx - 1) + 1):(100 * (minibatch_idx - 1) + 100));
+            end
+            
+            configure = [D,K];
+            
+            perception = ML.Perception(configure);
+            perception = perception.initialize();
+            perception = perception.train(data,1e-6,0.1,2e4);
+            
+            save('perception.mat','perception');
+            
+            y = perception.do(daata) > 0.5;
+            e = sum(xor(y,label)) / length(y);
+        end
+        
+        function [perception,e] = unit_test3()
+            clear all;
+            close all;
+            M = 100; S = 100; N = M * S;
+            r1 = 2 * rand(1,N) + 8; a1 =  pi * rand(1,N); group1 = repmat(r1,2,1) .* [cos(a1); sin(a1)];
+            r2 = 2 * rand(1,N) + 8; a2 = -pi * rand(1,N); group2 = repmat(r2,2,1) .* [cos(a2); sin(a2)];
+            group1(1,:) = group1(1,:) + 4; group1(2,:) = group1(2,:) - 2;   
+            group2(1,:) = group2(1,:) - 4; group2(2,:) = group2(2,:) + 2; 
+            figure(1);
+            plot(group1(1,:),group1(2,:),'+'); hold on;
+            plot(group2(1,:),group2(2,:),'o'); hold off;
+            
+            for minibatch_idx = 1:M
+                data{minibatch_idx}.labels = [ones(1,S) zeros(1,S)];
+                data{minibatch_idx}.points = [group1(:,(S*(minibatch_idx-1)+1):(S*(minibatch_idx-1)+S)) ...
+                                              group2(:,(S*(minibatch_idx-1)+1):(S*(minibatch_idx-1)+S))];
+            end
+            
+            configure = [2,20,1];
+            perception = ML.Perception(configure);
+            perception = perception.initialize();
+            perception = perception.train(data,1e-6,0.1,1e5);
+            
+            save('perception.mat','perception');
+            
+            daata = [group1 group2];
+            label = [ones(1,N) zeros(1,N)];
+            y = perception.do(daata) > 0.5;
+            e = sum(xor(y,label)) / length(y);
         end
     end
 end
