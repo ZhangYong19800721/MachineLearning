8fddf9215142e94ec69f7a5b57e5970e06ca86e4
ZhangYong19800721 <24452861@qq.com>
2017-08-05 20:54:16 +0800

    update

--- +learn/GaussianRBM.m
+++ +learn/GaussianRBM.m
@@ -8,7 +8,7 @@ classdef GaussianRBM
         weight;     % 权值矩阵(num_hidden * num_visual)
         hidden_bias; % 隐藏神经元的偏置
         visual_bias; % 可见神经元的偏置
-        sigma;       % 显层神经元加性高斯噪声的标准差
+        visual_sgma; % 显层神经元加性高斯噪声的标准差
     end
     
     methods
@@ -18,31 +18,33 @@ classdef GaussianRBM
             obj.weight = zeros(obj.num_hidden,obj.num_visual);
             obj.hidden_bias = zeros(obj.num_hidden,1);
             obj.visual_bias = zeros(obj.num_visual,1);
+            obj.visual_sgma = ones(obj.num_visual,1); % 将显层神经元的标准差初始化为1
         end
     end
     
     methods
-        function obj = construct(obj,weight,visual_bias,hidden_bias)
-            %construct 使用权值、隐神经元偏置、显神经元偏置直接构建RBM
-            [obj.num_hidden, obj.num_visual] = size(weight);
-            obj.weight = weight;
-            obj.hidden_bias = hidden_bias;
-            obj.visual_bias = visual_bias;
-        end
+%         function obj = construct(obj,weight,visual_bias,hidden_bias,visual_sgma)
+%             %construct 使用权值、隐神经元偏置、显神经元偏置直接构建RBM
+%             [obj.num_hidden, obj.num_visual] = size(weight);
+%             obj.weight = weight;
+%             obj.hidden_bias = hidden_bias;
+%             obj.visual_bias = visual_bias;
+%             obj.visual_sgma = visual_sgma;
+%         end
         
-        function obj = initialize(obj,minibatchs,visual_bias,hidden_bias) 
+        function obj = initialize(obj,minibatchs) 
             %initialize 基于训练数据（由多个minibatch组成的训练数据集合）初始化权值矩阵，隐神经元偏置和显神经元偏置
             %           minibatchs 是一个元胞数组。
            
             minibatch_num = length(minibatchs);
-            minibatch_sum = zeros(size(minibatchs{1}));
+            minibatch_ave = zeros(size(minibatchs{1}));
             
             for n = 1:minibatch_num
-                minibatch_sum = minibatch_sum + minibatchs{n};
+                minibatch_ave = minibatch_ave + minibatchs{n};
             end
             
-            minibatch_sum = minibatch_sum ./ minibatch_num;
-            obj = obj.initialize_weight(minibatch_sum,visual_bias,hidden_bias);
+            minibatch_ave = minibatch_ave ./ minibatch_num;
+            obj = obj.initialize_weight(minibatch_ave);
         end
 
         function obj = pretrain(obj,minibatchs,learn_rate_min,learn_rate_max,max_it) 
@@ -58,6 +60,7 @@ classdef GaussianRBM
             v_weight = zeros(size(obj.weight));
             v_h_bias = zeros(size(obj.hidden_bias));
             v_v_bias = zeros(size(obj.visual_bias));
+            v_v_sgma = zeros(size(obj.visual_sgma));
             
             % 初始化动量倍率为0.5
             momentum = 0.5;
@@ -65,7 +68,7 @@ classdef GaussianRBM
             r_error_list = zeros(1,ob_window_size);
             for idx = 1:minibatch_num  % 初始化重建误差列表的移动平均值
                 minibatch = minibatchs{idx};
-                [~, ~, ~, r_error] = obj.CD1(minibatch);
+                [~, ~, ~, ~, r_error] = obj.CD1(minibatch);
                 r_error_list(idx) = r_error;
             end
             r_error_ave_old = mean(r_error_list);
@@ -77,7 +80,7 @@ classdef GaussianRBM
                 minibatch_idx = mod(it,minibatch_num)+1;  % 取一个minibatch
                 minibatch = minibatchs{minibatch_idx};
                 
-                [d_weight, d_h_bias, d_v_bias, r_error] = obj.CD1(minibatch);
+                [d_weight, d_h_bias, d_v_bias, d_v_sgma, r_error] = obj.CD1(minibatch);
                 r_error_list(minibatch_idx) = r_error;
                 r_error_ave_new = mean(r_error_list);
                 
@@ -99,48 +102,51 @@ classdef GaussianRBM
                 v_weight = momentum * v_weight + learn_rate * d_weight;
                 v_h_bias = momentum * v_h_bias + learn_rate * d_h_bias;
                 v_v_bias = momentum * v_v_bias + learn_rate * d_v_bias;
+                v_v_sgma = momentum * v_v_sgma + learn_rate * d_v_sgma;
                 
                 obj.weight      = obj.weight      + v_weight;
                 obj.hidden_bias = obj.hidden_bias + v_h_bias;
                 obj.visual_bias = obj.visual_bias + v_v_bias;
+                obj.visual_sgma = obj.visual_sgma + v_v_sgma;
+                obj.visual_sgma(obj.visual_sgma<=0) = eps;
             end
         end
         
-        function h_state = posterior_sample(obj,v_state)
-            % posterior_sample 计算后验概率采样
-            % 在给定显层神经元取值的情况下，对隐神经元进行抽样
-            h_state = learn.sample(obj.posterior(v_state));
-        end
+%         function h_state = posterior_sample(obj,v_state)
+%             % posterior_sample 计算后验概率采样
+%             % 在给定显层神经元取值的情况下，对隐神经元进行抽样
+%             h_state = learn.sample(obj.posterior(v_state));
+%         end
+%         
+%         function h_field = posterior(obj,v_state) 
+%             %POSTERIOR 计算后验概率
+%             % 在给定显层神经元取值的情况下，计算隐神经元的激活概率
+%             h_field = learn.sigmoid(obj.foreward(v_state));
+%         end
+%         
+%         function v_state = likelihood_sample(obj,h_state) 
+%             % likelihood_sample 计算似然概率采样
+%             % 在给定隐层神经元取值的情况下，对显神经元进行抽样
+%             v_state = learn.sample(obj.likelihood(h_state));
+%         end
+%         
+%         function v_field = likelihood(obj,h_state) 
+%             % likelihood 计算似然概率
+%             % 在给定隐层神经元取值的情况下，计算显神经元的激活概率
+%             v_field = learn.sigmoid(obj.backward(h_state));
+%         end
         
-        function h_field = posterior(obj,v_state) 
-            %POSTERIOR 计算后验概率
-            % 在给定显层神经元取值的情况下，计算隐神经元的激活概率
-            h_field = learn.sigmoid(obj.foreward(v_state));
-        end
-        
-        function v_state = likelihood_sample(obj,h_state) 
-            % likelihood_sample 计算似然概率采样
-            % 在给定隐层神经元取值的情况下，对显神经元进行抽样
-            v_state = learn.sample(obj.likelihood(h_state));
-        end
-        
-        function v_field = likelihood(obj,h_state) 
-            % likelihood 计算似然概率
-            % 在给定隐层神经元取值的情况下，计算显神经元的激活概率
-            v_field = learn.sigmoid(obj.backward(h_state));
-        end
-        
-        function y = foreward(obj,x)
-            y = obj.weight * x + repmat(obj.hidden_bias,1,size(x,2));
-        end
-        
-        function x = backward(obj,y)
-            x = obj.weight'* y + repmat(obj.visual_bias,1,size(y,2));
-        end
+%         function y = foreward(obj,x)
+%             y = obj.weight * x + repmat(obj.hidden_bias,1,size(x,2));
+%         end
+%         
+%         function x = backward(obj,y)
+%             x = obj.weight'* y + repmat(obj.visual_bias,1,size(y,2));
+%         end
     end
     
     methods (Access = private)
-        function [d_weight,d_h_bias,d_v_bias,r_error] = CD1(obj, minibatch)
+        function [d_weight,d_h_bias,d_v_bias,d_v_sgma,r_error] = CD1(obj, minibatch)
             % 使用Contrastive Divergence 1 (CD1)方法对约束玻尔兹曼RBM机进行快速训练
             % 输入：
             %   minibatch，一组训练数据，一列代表一个训练样本，列数表示训练样本的个数
@@ -153,47 +159,48 @@ classdef GaussianRBM
             N = size(minibatch,2); % 训练样本的个数
             h_bias = repmat(obj.hidden_bias,1,N);
             v_bias = repmat(obj.visual_bias,1,N);
+            v_sgma = repmat(obj.visual_sgma,1,N);
             
-            h_field_0 = learn.sigmoid(obj.weight * minibatch + h_bias);
+            h_field_0 = learn.sigmoid(obj.weight * (minibatch ./ v_sgma) + h_bias);
             h_state_0 = learn.sample(h_field_0);
-            v_field_1 = learn.sigmoid(obj.weight'* h_state_0 + v_bias);
-            v_state_1 = learn.sample(v_field_1);
-            h_field_1 = learn.sigmoid(obj.weight * v_state_1 + h_bias);
+            v_field_1 = v_sgma .* (obj.weight'* h_state_0) + v_bias;
+            v_state_1 = v_field_1 + v_sgma .* randn(size(v_sgma));
+            h_field_1 = learn.sigmoid(obj.weight * (v_state_1 ./ v_sgma) + h_bias);
             
             r_error =  sum(sum(abs(v_field_1 - minibatch))) / N; %计算在整个minibatch上的平均重建误差
             
-            d_weight = (h_field_0 * minibatch' - h_field_1 * v_state_1') / N;
+            d_weight = (h_field_0 * (minibatch ./ v_sgma)' - h_field_1 * (v_state_1 ./ v_sgma)') / N;
             d_h_bias = (h_state_0 - h_field_1) * ones(N,1) / N;
-            d_v_bias = (minibatch - v_field_1) * ones(N,1) / N;
+            d_v_bias = ((minibatch - v_field_1) ./ v_sgma) * ones(N,1) / N;
+            d_v_sgma = (sum(((minibatch - v_bias).^2) ./ (v_sgma.^3),2)) / N - sum(((minibatch ./ v_sgma.^2) * h_field_0') .* obj.weight',2) / N ...
+                     - (sum(((v_state_1 - v_bias).^2) ./ (v_sgma.^3),2)) / N + sum(((v_state_1 ./ v_sgma.^2) * h_field_1') .* obj.weight',2) / N;
         end
         
-        function obj = initialize_weight(obj,train_data,visual_bias,hidden_bias)
+        function obj = initialize_weight(obj,train_data)
             %INTIALIZE 初始化权值矩阵为0附近的小随机数，初始化显层神经元的偏置为先验概率，初始化隐层神经元的偏置为0.
             obj.weight = 0.01 * randn(size(obj.weight));
-            x = sum(train_data,2) / size(train_data,2);
-            x(x<=0) = x(x<=0) + 10.^visual_bias;
-            x(x>=1) = x(x>=1) - 10.^visual_bias;
-            obj.visual_bias = log(x./(1-x));
-            obj.hidden_bias = hidden_bias * ones(size(obj.hidden_bias));
+            obj.visual_bias = sum(train_data,2) / size(train_data,2);
+            obj.hidden_bias = zeros(size(obj.hidden_bias));
         end
     end
     
     methods(Static)
-        function [rbm,e] = unit_test()
+        function [grbm,e] = unit_test()
             clear all;
             close all;
             [data,~,~,~] = learn.import_mnist('./+learn/mnist.mat');
+            data = 255 * data;
             [D,S,M] = size(data);
             
             for minibatch_idx = 1:M
                 mnist{minibatch_idx} = data(:,:,minibatch_idx);
             end
             
-            rbm = learn.RestrictedBoltzmannMachine(D,500);
-            rbm = rbm.initialize(mnist,-6,0);
-            rbm = rbm.pretrain(mnist,1e-6,1e-1,1e5);
+            grbm = learn.GaussianRBM(D,500);
+            grbm = grbm.initialize(mnist);
+            grbm = grbm.pretrain(mnist,1e-6,1e-3,1e6);
             
-            save('rbm.mat','rbm');
+            save('grbm.mat','grbm');
             
             e = 1;
         end
