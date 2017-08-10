e7743a31eee9a0c8a9b9f5542defce63e06357e5
root <root>
2017-08-10 15:34:38 +0800

    update

--- main.m
+++ main.m
@@ -9,28 +9,23 @@ nucleotide = nucleotide(:,shuffle_idx);
 S = 100; M = floor(N / S); N = S*M;
 nucleotide = nucleotide(:,1:N);
 
-[W,R,A] = learn.whiten(nucleotide);
-data = W * (nucleotide - repmat(A,1,size(nucleotide,2)));
-
-ave_value = mean(data,2);
-std_value = std(data,0,2);
-
-data = reshape(data,D,S,M);
+nucleotide = reshape(nucleotide,D,S,M);
 
 gbrbm = learn.GBRBM(D,1024);
-gbrbm = gbrbm.initialize(data);
+gbrbm = gbrbm.initialize(nucleotide);
 
-parameters.learn_rate = [1e-8,1e-4];
+parameters.learn_rate = [1e-10,1e-3];
 parameters.learn_sgma = 1e-2;
 parameters.weight_cost = 1e-4;
 parameters.max_it = 1e6;
 
-gbrbm = gbrbm.pretrain(data,parameters);
-recon_data = gbrbm.reconstruct(data);
-recon_nucleotide = R * recon_data + repmat(A,1,size(recon_data,2));
+gbrbm = gbrbm.pretrain(nucleotide,parameters);
+nucleotide = reshape(nucleotide,D,[]);
+recon_nucleotide = gbrbm.reconstruct(nucleotide);
 
-nucleotide = uint8(nucleotide);
-recon_nucleotide = uint8(recon_nucleotide);
+nucleotide = round(nucleotide);
+recon_nucleotide = round(recon_nucleotide); 
+recon_nucleotide(recon_nucleotide<0) = 0;
 
 error = sum(sum((nucleotide - recon_nucleotide).^2)) / N;
 
