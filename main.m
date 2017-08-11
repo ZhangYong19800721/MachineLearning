eaaba0af5aeecee4764d1af028f2555c19352f00
root <root>
2017-08-11 15:02:39 +0800

    update

--- main.m
+++ main.m
@@ -2,31 +2,26 @@ clear all;
 close all;
 
 load('nucleotides.mat');
-
 [D,N] = size(nucleotide);
-shuffle_idx = randperm(N);
-nucleotide = nucleotide(:,shuffle_idx);
 S = 100; M = floor(N / S); N = S*M;
 nucleotide = nucleotide(:,1:N);
-
+nucleotide(nucleotide>0) = 1;
 nucleotide = reshape(nucleotide,D,S,M);
 
-gbrbm = learn.GBRBM(D,1024);
-gbrbm = gbrbm.initialize(nucleotide);
+rbm = learn.RBM(D,1024);
+rbm = rbm.initialize(nucleotide);
 
-parameters.learn_rate = [1e-10,1e-4];
-parameters.learn_sgma = 1e-2;
+parameters.learn_rate = [1e-8,1e-2];
 parameters.weight_cost = 1e-4;
 parameters.max_it = 1e6;
 
-gbrbm = gbrbm.pretrain(nucleotide,parameters);
+rbm = rbm.pretrain(nucleotide,parameters);
 nucleotide = reshape(nucleotide,D,[]);
-recon_nucleotide = gbrbm.reconstruct(nucleotide);
+recon_nucleotide = rbm.reconstruct(nucleotide);
 
-nucleotide = round(nucleotide);
 recon_nucleotide = round(recon_nucleotide); 
 recon_nucleotide(recon_nucleotide<0) = 0;
 
 error = sum(sum((nucleotide - recon_nucleotide).^2)) / N;
 
-save('gbrbm_nucleotide.mat');
+save;
