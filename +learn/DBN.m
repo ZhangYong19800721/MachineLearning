8b5680460e0573210ad33eb1738985f7b5b8fb53
ZhangYong19800721 <24452861@qq.com>
2017-08-15 17:09:10 +0800

    update

--- +learn/DBN.m
+++ +learn/DBN.m
@@ -15,22 +15,6 @@ classdef DBN
     end
     
     methods        
-        function h_field = posterior(obj,v_state)
-            % posterior 给定显层神经元的取值，计算顶层隐藏神经元的域值
-            h_field = v_state;
-            for layer_idx = 1:length(obj.rbm_layers)
-                h_field = obj.rbm_layers{layer_idx}.posterior(h_field);
-            end
-        end
-        
-        function v_field = likelihood(obj,h_state)
-            % likelihood 给定顶层隐神经元的取值，计算底层显神经元的域值
-            v_field = h_state;
-            for layer_idx = length(obj.rbm_layers):-1:1
-                v_field = obj.rbm_layers{layer_idx}.likelihood(v_field);
-            end
-        end
-        
         function obj = pretrain(obj,minibatchs,labels,parameters)
             %pretrain 预训练
             % 使用CD1快速算法，逐层训练约束玻尔兹曼机（RBM）           
@@ -47,14 +31,33 @@ classdef DBN
             %train UP-DOWN训练算法
             %           
             [D,S,M] = size(minibatchs); [K,~,~] = size(labels); L = obj.stacked_rbm.layer_num();
-            obj.stacked_rbm_gen = obj.stacked_rbm; % 权值解锁
+            ob = learn.Observer('重建误差',1,M); %初始化观察者，观察重建误差
+            
+            obj.stacked_rbm = obj.stacked_rbm.weightsync(); % 权值同步/解锁
+            % obj.softmax_rbm = obj.softmax_rbm.weightsync(); % 权值同步/解锁
+            
             learn_rate_min = min(parameters.learn_rate);
             learn_rate     = max(parameters.learn_rate);
+            
+            recon_error_list = zeros(1,M);
+            for m = 1:M  % 初始化重建误差列表的移动平均值
+                minibatch = minibatchs(:,:,m); label = labels(:,:,m);
+                [recon_label,recon_data] = obj.rebuild(label,minibatch);
+                recon_error = sum(sum(([recon_label;recon_data] - [label;minibatch]).^2)) / S;
+                recon_error_list(m) = recon_error;
+            end
+            recon_error_ave_old = mean(recon_error_list);
+            ob = ob.initialize(recon_error_ave_old);
             
             for it = 0:parameters.max_it
                 m = mod(it,M)+1;
                 label = labels(:,:,m);
                 minibatch = minibatchs(:,:,m);
+                
+                [recon_label,recon_data] = obj.rebuild(label,minibatch);
+                recon_error = sum(sum(([recon_label;recon_data] - [label;minibatch]).^2)) / S;
+                recon_error_list(m) = recon_error;
+                recon_error_ave_new = mean(recon_error_list);
                 
                 if m == M % 当所有的minibatch被轮讯了一篇的时候（到达观察窗口最右边的时候）
                     if recon_error_ave_new > recon_error_ave_old
@@ -70,7 +73,7 @@ classdef DBN
                 description = strcat(description,strcat('迭代次数:',num2str(it)));
                 description = strcat(description,strcat('学习速度:',num2str(learn_rate)));
                 disp(description);
-                %ob = ob.showit(r_error_ave_new,description);
+                ob = ob.showit(r_error_ave_new,description);
                 
                 % positive phase (wake 阶段)
                 pos_state = obj.stacked_rbm.posterior_sample(minibatch);
@@ -86,39 +89,39 @@ classdef DBN
                 neg_top_hid_state = learn.sample(neg_top_hid_proba);
                 
                 % negative phase (sleep 阶段)
-                neg_state = obj.stacked_rbm_gen.likelihood_sample(neg_top_vis_state);
+                neg_state = obj.stacked_rbm.likelihood_sample(neg_top_vis_state);
                 neg_state{1}.state = neg_state{1}.proba;
                 
                 % prediction
                 for l = L:-1:1
-                    pre_neg_proba{l+1} = obj.stacked_rbm.rbms{l}.posterior(neg_state{l}.state);
-                    pre_pos_proba{l} = obj.stacked_rbm_gen.rbms{l}.likelihood(pos_state{l+1}.state);
+                    pre_neg_proba{l+1} = obj.stacked_rbm.rbms{l}.posterior (neg_state{l  }.state);
+                    pre_pos_proba{l  } = obj.stacked_rbm.rbms{l}.likelihood(pos_state{l+1}.state);
                 end
                 
                 % 更新产生权值
                 for l = 1:L
-                    obj.stacked_rbm_gen.rbms{l}.weight = obj.stacked_rbm_gen.rbms{l}.weight + learn_rate * ...
-                        pos_state{l+1}.state * (pos_state{l}.state - pre_pos_proba{l})' / S;
-                    obj.stacked_rbm_gen.rbms{l}.visual_bias = obj.stacked_rbm_gen.rbms{l}.visual_bias + learn_rate * ...
-                        sum(pos_state{l}.state - pre_pos_proba{l},2) / S; 
+                    obj.stacked_rbm.rbms{l}.weight_h2v  = obj.stacked_rbm.rbms{l}.weight_h2v  + learn_rate * pos_state{l+1}.state * (pos_state{l}.state - pre_pos_proba{l})' / S;
+                    obj.stacked_rbm.rbms{l}.visual_bias = obj.stacked_rbm.rbms{l}.visual_bias + learn_rate * sum(pos_state{l}.state - pre_pos_proba{l},2) / S; 
                 end
                 
                 % 更新顶层权值
-                obj.softmax_rbm.weight = obj.softmax_rbm.weight + learn_rate * ...
-                    (pos_top_hid_state * [label; pos_state{L+1}.state]' - neg_top_hid_state * [neg_top_lab_state; neg_top_vis_state]') / S;
-                obj.softmax_rbm.visual_bias = obj.softmax_rbm.visual_bias + learn_rate * ...
-                    sum([label;pos_state{L+1}.state] - [neg_top_lab_state;neg_top_vis_state],2) / S;
-                obj.softmax_rbm.hidden_bias = obj.softmax_rbm.hidden_bias + learn_rate * ...
-                    sum(pos_top_hid_state - neg_top_hid_state,2) / S;
+                obj.softmax_rbm.weight_s2h   = obj.softmax_rbm.weight_s2h   + learn_rate * (pos_top_hid_state * label'                - neg_top_hid_state * neg_top_lab_state') / S;
+                obj.softmax_rbm.weight_v2h   = obj.softmax_rbm.weight_v2h   + learn_rate * (pos_top_hid_state * pos_state{L+1}.state' - neg_top_hid_state * neg_top_vis_state') / S;
+                obj.softmax_rbm.softmax_bias = obj.softmax_rbm.softmax_bias + learn_rate * sum(label                - neg_top_lab_state,2) / S;
+                obj.softmax_rbm.visual_bias  = obj.softmax_rbm.visual_bias  + learn_rate * sum(pos_state{L+1}.state - neg_top_vis_state,2) / S;
+                obj.softmax_rbm.hidden_bias  = obj.softmax_rbm.hidden_bias  + learn_rate * sum(pos_top_hid_state    - neg_top_hid_state,2) / S;
                 
                 % 更新识别权值
                 for l = 1:L
-                    obj.stacked_rbm.rbms{l}.weight = obj.stacked_rbm.rbms{l}.weight + learn_rate * ...
-                        (neg_state{l+1}.state - pre_neg_proba{l+1}) * neg_state{l}.state' / S;
-                    obj.stacked_rbm.rbms{l}.hidden_bias = obj.stacked_rbm.rbms{l}.hidden_bias + learn_rate * ...
-                        sum(neg_state{l+1}.state - pre_neg_proba{l+1},2) / S;
+                    obj.stacked_rbm.rbms{l}.weight_v2h  = obj.stacked_rbm.rbms{l}.weight_v2h  + learn_rate * (neg_state{l+1}.state - pre_neg_proba{l+1}) * neg_state{l}.state' / S;
+                    obj.stacked_rbm.rbms{l}.hidden_bias = obj.stacked_rbm.rbms{l}.hidden_bias + learn_rate * sum(neg_state{l+1}.state - pre_neg_proba{l+1},2) / S;
                 end
             end
+        end
+        
+        function [c,y] = rebuild(obj,s,x)
+            x = obj.stacked_rbm.posterior(x);
+            [c,y] = obj.softmax_rbm.rebuild(s,x);
         end
         
         function y = classify(obj,x)
--- main3.m
+++ main3.m
@@ -11,10 +11,10 @@ dbn = learn.DBN(configure);
 
 parameters.learn_rate = [1e-8 1e-2];
 parameters.weight_cost = 1e-4;
-parameters.max_it = 1e0;
+parameters.max_it = 1e6;
 dbn = dbn.pretrain(train_images,train_labels,parameters);
 
-load('dbn_pretrain.mat');
+save('dbn_pretrain.mat','dbn');
 
 y = dbn.classify(test_images);
 error1 = sum(y~=test_labels') / length(y);
