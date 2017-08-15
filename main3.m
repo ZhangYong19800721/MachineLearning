c5605ea086b920cf2e379ac97b8f9b8ac94a0be0
ZhangYong19800721 <24452861@qq.com>
2017-08-15 16:13:55 +0800

    update

---
+++ main3.m
@@ -0,0 +1,25 @@
+clear all;
+close all;
+
+[train_images,~,test_images,test_labels] = learn.import_mnist('./+learn/mnist.mat');
+K = 10; [D,S,M] = size(train_images);
+train_labels = eye(10); train_labels = repmat(train_labels,1,10,M);
+
+configure.stacked_rbm = [D,500,500];
+configure.softmax_rbm = [K,500,2000];
+dbn = learn.DBN(configure);
+
+parameters.learn_rate = [1e-8 1e-2];
+parameters.weight_cost = 1e-4;
+parameters.max_it = 1e0;
+dbn = dbn.pretrain(train_images,train_labels,parameters);
+
+load('dbn_pretrain.mat');
+
+y = dbn.classify(test_images);
+error1 = sum(y~=test_labels') / length(y);
+
+dbn = dbn.train(train_images,train_labels,parameters);
+save('dbn_train.mat','dbn');
+y = dbn.classify(test_images);
+error2 = sum(y~=test_labels') / length(y);
