916b4c6a5b3c7a9e8dd906e164d70adb28c8fc89
ZhangYong19800721 <24452861@qq.com>
2017-08-29 20:22:16 +0800

    update

--- +learn/RealBoost.m
+++ +learn/RealBoost.m
@@ -13,16 +13,7 @@ classdef RealBoost
         end
     end
     
-    methods(Access = public)
-        function y = predict(obj,points)  
-            % PREDICT 使用经过训练的模型判断数据点的分类，正例为1，反例为0
-            %
-            H = length(obj.weak); [~,N] = size(points); % H弱分类器的个数，N数据点数
-            f = zeros(H,N);                             % 存储弱分类器的计算结果
-            for h=1:H,f(h,:) = obj.weak{h}.predict(points);end
-            y = sum(f) > 0;
-        end
-        
+    methods(Access = private)
         function [obj,w]  = ensemble(obj,points,labels,weight,wc)
             %ensemble 组合弱分类器，找到一个最优的弱分类器，然后通过此函数组合起来
             %   输入：
@@ -35,10 +26,83 @@ classdef RealBoost
             %   obj 训练后的adaboost对象
             %   w 更新后的权值
             
-            f = wc.predict(points); % 计算加权激活概率
-            obj.weak{1+length(obj.weak)} = wc;
-            w = weight .* exp(-labels .* f);
-            w = w ./ sum(w); 
+            f = wc.predict(points); % 计算弱分类器函数值f(x)
+            obj.weak{1+length(obj.weak)} = wc; % 将弱分类器加入组中
+            w = weight .* exp(-labels .* f); % 更新权值
+            w = w ./ sum(w); % 归一化权值
+        end
+        
+        function wc = find_wc(obj,points,labels,weight)
+            % 选择最优的弱分类器
+            wc = learn.Weak_LineR(); l = labels > 0;
+            [~,N] = size(points); epsilon = 1/N;
+            
+            X = -15:15; Y = -15:15; A = linspace(-pi/2,pi/2,180); 
+            zm = inf;
+            for x = X 
+                for y = Y
+                    for a = A
+                        v = [-tan(a) 1 tan(a)*x-y];
+                        wc.w = [v(1) v(2)]; wc.b = v(3);
+                        p = wc.predict(points);
+                        W_POS_1 = sum(weight( l &  p));
+                        W_NEG_1 = sum(weight(~l &  p));
+                        W_POS_2 = sum(weight( l & ~p));
+                        W_NEG_2 = sum(weight(~l & ~p));
+                        z = 2 * sum(sqrt(W_POS_1 * W_NEG_1) + sqrt(W_POS_2 * W_NEG_2));
+                        if z < zm
+                            best.w = wc.w;
+                            best.b = wc.b;
+                            best.x = 0.5 * log((W_POS_1 + epsilon) / (W_NEG_1 + epsilon));
+                            best.y = 0.5 * log((W_POS_2 + epsilon) / (W_NEG_2 + epsilon));
+                            zm = z;
+                        end
+                    end
+                end
+            end
+            
+            wc.w = best.w;
+            wc.b = best.b;
+            wc.x = best.x;
+            wc.y = best.y;
+        end
+    end
+    
+    methods(Access = public)
+        function y = compute(obj,points)
+            % compute 计算boost模型的响应函数F(x)
+            M = length(obj.weak); [~,N] = size(points); % M弱分类器的个数，N数据点数
+            f = zeros(M,N);                             % 存储弱分类器的计算结果
+            for m=1:M,f(m,:) = obj.weak{m}.predict(points);end
+            y = sum(f);
+        end
+        
+        function y = predict(obj,points)  
+            % PREDICT 使用经过训练的模型判断数据点的分类，正例为1，反例为0
+            y = obj.compute(points) > 0;
+        end
+        
+        function obj = train(obj,points,labels,M)
+            % train 训练RealBoost模型
+            % 输入：
+            % points 数据点
+            % labels 标签，+1或-1
+            % M 弱分类器的个数
+            % 输出：
+            % obj 经过训练的boost对象
+            
+            [~,N] = size(points); % 得到数据点数目
+            weight = ones(1,N) / N; % 初始化权值
+            for m = 1:M
+                wc = obj.find_wc(points,labels,weight);
+                [obj,weight] = obj.ensemble(points,labels,weight,wc);
+                
+                a = obj.weak{m}.w(1);
+                b = obj.weak{m}.w(2);
+                q = obj.weak{m}.b;
+                myfunc = @(x,y) a*x + b*y + q;
+                ezplot(myfunc,[-15,15,-8,8]);
+            end
         end
     end
     
@@ -50,7 +114,7 @@ classdef RealBoost
             
             boost = learn.RealBoost();
             
-            N = 1e4; epsilon = 1/N;
+            N = 1e4;
             [points,labels] = learn.GenerateData.type1(N); l = labels > 0;
             
             figure;
@@ -59,57 +123,11 @@ classdef RealBoost
             plot(group1(1,:),group1(2,:),'+'); hold on;
             plot(group2(1,:),group2(2,:),'*'); 
             
-            weight = ones(1,N)/N; % 初始化权值
-           
-            [X,Y] = meshgrid(-15:15,-15:15); A = (-pi/2+eps):0.1:(pi/2-eps); 
-            X = reshape(X,1,[]); Y = reshape(Y,1,[]);
-            T = 10;
+            M = 5;
+            boost = boost.train(points,labels,M);
             
-            for t = 1:T
-                z_min = inf; best_w = []; best_b = [];
-                wc = learn.Weak_LineR();
-                
-                for i = 1:length(X)
-                    for j = 1:length(A)
-                        v = [tan(A(j)) 1 -(tan(A(j))*X(i)+Y(i))];
-                        wc.w = [v(1) v(2)]; wc.b = v(3);
-                        p = wc.predict(points);
-                        W_POS_1 = sum(weight( l &  p));
-                        W_NEG_1 = sum(weight(~l &  p));
-                        W_POS_2 = sum(weight( l & ~p));
-                        W_NEG_2 = sum(weight(~l & ~p));
-                        z = 2 * sum(sqrt(W_POS_1 * W_NEG_1) + sqrt(W_POS_2 * W_NEG_2));
-                        
-                        if z < z_min
-                            best_w = wc.w;
-                            best_b = wc.b;
-                            best_x = 0.5 * log((W_POS_1 + epsilon) / (W_NEG_1 + epsilon));
-                            best_y = 0.5 * log((W_POS_2 + epsilon) / (W_NEG_2 + epsilon));
-                            z_min = z;
-                        end
-                    end
-                end
-                
-                wc.w = best_w; wc.b = best_b; wc.x = best_x; wc.y = best_y;
-                [boost,weight] = boost.ensemble(points,labels,weight,wc);
-                
-                a = boost.weak{t}.w(1);
-                b = boost.weak{t}.w(2);
-                q = boost.weak{t}.b;
-                myfunc = @(x,y) a*x + b*y + q;
-                ezplot(myfunc,[-15,15,-8,8]);
-            end
-            
-%             for t = 1:T
-%                 a = boost.weak{t}.w(1);
-%                 b = boost.weak{t}.w(2);
-%                 q = boost.weak{t}.b;
-%                 myfunc = @(x,y) a*x + b*y + q;
-%                 ezplot(myfunc,[-15,15,-8,8]);
-%             end
-            
-            c = boost.predict(points);
-            error = sum(xor(c,l)) / N
+            y = boost.predict(points);
+            error = sum(xor(y,l)) / N
         end
     end
 end
