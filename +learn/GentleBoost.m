71ab8c54c16251bcdfa98616a7f95819b08ee77d
ZhangYong19800721 <24452861@qq.com>
2017-08-29 18:16:50 +0800

    update

--- +learn/GentleBoost.m
+++ +learn/GentleBoost.m
@@ -1,11 +1,14 @@
 classdef GentleBoost
     %GENTLEBOOST GentleBoost算法
-    %  参考“Robust Real-time Object Detection”
+    %  参考论文"Additive Logistic Regression a Statistical View of Boosting"
+    %  这篇论文非常重要，揭示了boost算法和加性统计模型在本质上是相同的，分析
+    %  了AdaBoost、RealBoost、LogitBoost、GentleBoost算法。
     
     properties
+        weak; % 包含若干个弱分类器的cell数组
     end
     
-    methods
+    methods(Access = private)
         function [obj,w]  = ensemble(obj,points,labels,weight,wc)
             %ensemble 组合弱分类器
             %   输入：
@@ -18,22 +21,40 @@ classdef GentleBoost
             %   obj 训练后的adaboost对象
             %   w 更新后的权值
             
-            c = wc.predict(points); % 使用弱分类器对所有的数据点进行分类
-            epsilon = sum(weight .* abs(c - labels)); % 计算在weight分布条件下的分类误差
-            beda = epsilon / (1 - epsilon); 
-            obj.weak{1+length(obj.weak)} = wc;
-            obj.alfa = [obj.alfa log(1/beda)];
-            w = weight .* (beda.^(1 - c~=labels)); % 更新权值
-            w = w ./ sum(w);
+            f = wc.predict(points); % 首先计算弱分类器的输出
+            obj.weak{1+length(obj.weak)} = wc; % 将弱分类器加入到boost组中
+            w = weight .* (-labels .* f); % 更新权值
+            w = w ./ sum(w); % 归一化权值
+        end
+    end
+    
+    methods(Access = public)
+        function y = compute(obj,points)
+            M = length(obj.weak); [~,N] = size(points); % M弱分类器的个数，N数据点数
+            f = logical(M,N); % 存储弱分类器的分类结果
+            for m=1:M, f(m,:) = obj.weak{m}.predict(points); end
+            y = sum(f);
         end
         
         function y = predict(obj,points)  % 使用经过训练的模型判断数据点的分类
             %PREDICT 对数据点进行分类，正例为1，反例为0
             %
-            H = length(obj.alfa); [~,N] = size(points); % H弱分类器的个数，N数据点数
-            c = logical(H,N); % 存储弱分类器的分类结果
-            for h=1:H, c(h,:) = obj.weak{h}.predict(points); end
-            y = obj.alfa * c >= sum(obj.alfa) / 2;
+            y = obj.compute(points) > 0;
+        end
+        
+        function obj = train(obj,points,labels,M)
+            % train 训练GentleBoost模型
+            % 输入：
+            % points 数据点
+            % labels 标签，+1或-1
+            % M 弱分类器的个数
+            % 输出：
+            % obj 经过训练的boost对象
+            
+            [~,N] = size(points); % 得到数据点数目
+            weight = ones(1,N) / N; % 初始化权值
+            
+            
         end
     end
 end
--- +learn/LogitBoost.m
+++ +learn/LogitBoost.m
@@ -1,26 +1,35 @@
-classdef RealAdaBoost
-    %RealAdaBoost 实现Real AdaBoost算法
+classdef LogitBoost
+    %LogitBoost 实现Logit Boost算法
     %  参考论文"Additve Logistic Regression a Statistical View of Boosting"
     %  这篇论文非常重要，揭示了boost算法和加性统计模型在本质上是相同的，分析
-    %  了RealAdaBoost和GentleBoost算法。
+    %  了AdaBoost、RealBoost、LogitBoost、GentleBoost算法。
+    %  代码未完成
+    TODO
     
     properties
         weak; % 包含若干个弱分类器的cell数组
     end
     
     methods
-        function obj = RealAdaBoost()
+        function obj = LogitBoost()
+        end
+    end
+    
+    methods(Access = private)
+        function y = compute(obj,points)  
+            % compute 计算F(x)的值
+            %
+            H = length(obj.weak); [~,N] = size(points); % H弱分类器的个数，N数据点数
+            f = zeros(H,N);                             % 存储弱分类器的计算结果
+            for h=1:H,f(h,:) = obj.weak{h}.predict(points);end
+            y = sum(f);
         end
     end
     
     methods(Access = public)
         function y = predict(obj,points)  
             % PREDICT 使用经过训练的模型判断数据点的分类，正例为1，反例为0
-            %
-            H = length(obj.weak); [~,N] = size(points); % H弱分类器的个数，N数据点数
-            f = zeros(H,N);                             % 存储弱分类器的计算结果
-            for h=1:H,f(h,:) = obj.weak{h}.predict(points);end
-            y = sum(f) > 0;
+            y = obj.compute(points) > 0;
         end
         
         function [obj,w]  = ensemble(obj,points,labels,weight,wc)
