2dc209ee63b9ec119eb81d78624d4ebba852ba01
ZhangYong19800721 <24452861@qq.com>
2017-08-31 14:49:35 +0800

    update

--- +learn/StumpSSC.m
+++ +learn/StumpSSC.m
@@ -6,18 +6,23 @@ classdef StumpSSC
         a; b; k; t;
     end
     
-    methods(Access = private)
+    methods(Access = public)
         function y = compare(obj,points1,points2)
             % compare 计算两个点之间的弱分类值
+            % 弱分类器判为正例时返回a+b
+            % 弱分类器判为反例时返回b
             c1 = points1(obj.k,:) > obj.t;
             c2 = points2(obj.k,:) > obj.t;
-            y = obj.a * xor(c1,c2) + obj.b;
+            y = obj.a * (c1==c2) + obj.b;
         end
         
         function y = compute(obj,points,paridx)
             % compute 计算两个点之间的弱分类值
+            % 弱分类器判为正例时返回a+b
+            % 弱分类器判为反例时返回b
             c = points(obj.k,:) > obj.t;
-            y = obj.a * xor(c(paridx(1,:)),c(paridx(2,:))) + obj.b;
+            c1 = c(paridx(1,:)); c2 = c(paridx(2,:));
+            y = obj.a * (c1==c2) + obj.b;
         end
     end
 end
