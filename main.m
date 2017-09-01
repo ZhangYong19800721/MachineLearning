e3a66c6260040597e73f6c3de0e3f6efc2014724
root <root>
2017-09-01 17:24:36 +0800

    update

--- main.m
+++ main.m
@@ -1,25 +1,10 @@
 clear all;
 close all;
 
+import_similarity_labels;
+
 load('nucleotides.mat');
-[D,N] = size(nucleotide);
-S = 100; M = floor(N / S); N = S*M;
-nucleotide = nucleotide(:,1:N);
-nucleotide(nucleotide>0) = 1;
-nucleotide = reshape(nucleotide,D,S,M);
 
-configure = [D,1024,256];
-sae = learn.SAE(configure);
+K = 1;
+subplot(2,1,1); bar(nucleotide(:,labels(1,K))); subplot(2,1,2); bar(nucleotide(:,labels(2,K)));
 
-parameters.learn_rate = [1e-6,1e-2];
-parameters.weight_cost = 1e-4;
-parameters.max_it = 1e6;
-sae = sae.pretrain(nucleotide,parameters);
-            
-nucleotide = reshape(nucleotide,D,[]);
-recon_nucleotide = sae.rebuild(nucleotide,'sample');
-recon_nucleotide = recon_nucleotide > 0.5;
-
-error = sum(sum((nucleotide - recon_nucleotide).^2)) / N;
-
-save('sae_pretrained_DAN.mat','sae');
