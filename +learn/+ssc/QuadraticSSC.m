bbfdd84ddf370c8abaabd8171f42df0f9cebd2f5
ZhangYong19800721 <24452861@qq.com>
2017-09-04 11:32:37 +0800

    update

--- +learn/+ssc/QuadraticSSC.m
+++ +learn/+ssc/QuadraticSSC.m
@@ -15,8 +15,8 @@ classdef QuadraticSSC
             % 弱分类器判为反例时返回b
             
             [~,N1] = size(points1); [~,N2] = size(points2); 
-            v1 = sum((points1' * obj.A) .* points1',2)' + obj.B * points1 + repmat(obj.C,1,N1);
-            v2 = sum((points2' * obj.A) .* points2',2)' + obj.B * points2 + repmat(obj.C,1,N2);
+            v1 = 0.5 * sum((points1' * obj.A) .* points1',2)' + obj.B * points1 + repmat(obj.C,1,N1);
+            v2 = 0.5 * sum((points2' * obj.A) .* points2',2)' + obj.B * points2 + repmat(obj.C,1,N2);
             c1 = v1 > 0;
             c2 = v2 > 0;
             y = obj.a * (c1==c2) + obj.b;
@@ -28,7 +28,7 @@ classdef QuadraticSSC
             % 弱分类器判为反例时返回b
             
             [~,N] = size(points); 
-            v = sum((points' * obj.A) .* points',2)' + obj.B * points + repmat(obj.C,1,N);
+            v = 0.5 * sum((points' * obj.A) .* points',2)' + obj.B * points + repmat(obj.C,1,N);
             c = v > 0; c1 = c(paridx(1,:)); c2 = c(paridx(2,:));
             y = obj.a * (c1==c2) + obj.b;
         end
