2ef936b9f3e6bbae8484f6482d5e08f6036998f8
ZhangYong19800721 <24452861@qq.com>
2017-09-05 18:53:31 +0800

    update

--- +learn/+optimal/maximize_cg.m
+++ +learn/+optimal/maximize_cg.m
@@ -3,11 +3,11 @@ function [x1,y1] = maximize_cg(F,x0,parameters)
 % 输入：
 %   F 函数对象，调用F.object(x)计算目标函数在x处的值，调用F.gradient(x)计算目标函数在x处的梯度
 %   x0 迭代的起始位置
-%   parameters.epsilon1 当梯度模小于epsilon1时停止迭代
-%   parameters.epsilon2 线性搜索的停止条件
+%   parameters.epsilon 当梯度模小于epsilon时停止迭代
+%   parameters.alfa 线性搜索区间倍数
+%   parameters.beda 线性搜索的停止条件
 %   parameters.max_it 最大迭代次数
 %   parameters.reset 重置条件
-%   parameters.dis 线性搜索的最大距离
 
     F = learn.optimal.NEGATIVE(F);
     [x1,y1] = learn.optimal.minimize_cg(F,x0,parameters);
