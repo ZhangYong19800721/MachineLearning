0c2376a79c50ff84facbeb6e6a9a9ab91bfe3b5b
ZhangYong19800721 <24452861@qq.com>
2017-09-05 09:24:57 +0800

    negative wrapper

---
+++ +learn/+optimal/NEGATIVE.m
@@ -0,0 +1,24 @@
+classdef NEGATIVE
+    %NEGATIVE NEGATIVE是一个包裹器，包裹F，其对目标函数和梯度的计算结果等于F取反
+    %
+    
+    properties
+        F;
+    end
+    
+    methods
+        function obj = NEGATIVE(F)
+            obj.F = F;
+        end
+        
+        function y = object(obj,x)
+            y = -obj.F.object(x);
+        end
+        
+        function g = gradient(obj,x)
+            g = -obj.F.gradient(x);
+        end
+    end
+    
+end
+
