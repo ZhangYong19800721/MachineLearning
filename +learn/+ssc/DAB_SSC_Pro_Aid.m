0dc01c4995469a7ece0f135c63c4a34292bfbaff
ZhangYong19800721 <24452861@qq.com>
2017-09-05 11:51:08 +0800

    update

--- +learn/+ssc/DAB_SSC_Pro_Aid.m
+++ +learn/+ssc/DAB_SSC_Pro_Aid.m
@@ -32,7 +32,7 @@ classdef DAB_SSC_Pro_Aid
             end
             g_h_x = repmat(h .* (h - 1),K*K+K+1,1) .* [g_h_A;g_h_B;g_h_C];
             g_c_x = 4 * (g_h_x(:,I) .* (g_h_x(:,J) - 0.5)) + 4 * (g_h_x(:,J) .* (g_h_x(:,I) - 0.5));
-            g = -sum(repmat(obj.weight .* L,K*K+K+1,1) .* g_c_x,2);
+            g = sum(repmat(obj.weight .* L,K*K+K+1,1) .* g_c_x,2);
         end
         
         function y = object(obj,x)
@@ -45,7 +45,7 @@ classdef DAB_SSC_Pro_Aid
             f = 0.5 * sum((obj.points' * A) .* obj.points',2)' + B * obj.points + repmat(C,1,N); % 计算所有点的f函数值
             h = +learn.tools.sigmoid(f); % 计算所有点的h函数值
             c = 4 * (h(I) - 0.5) .* (h(J) - 0.5); % 计算所有点的c函数值
-            y = -sum(obj.weight .* L .* c,2); % 计算目标函数值
+            y = sum(obj.weight .* L .* c,2); % 计算目标函数值
         end
     end
     
