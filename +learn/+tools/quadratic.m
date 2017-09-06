e0758551445459fee5efc5ff16eb19bcbf4bed58
ZhangYong19800721 <24452861@qq.com>
2017-09-06 15:16:44 +0800

    update

--- +learn/+tools/quadratic.m
+++ +learn/+tools/quadratic.m
@@ -1,6 +1,10 @@
 function y = quadratic(A,B,C,x)
-%quadratic 二次多项式函数的实现 
-%   y = 0.5*x'*A*x+B*x+C
+%quadratic 二次多项式函数的实现
+%   输入：
+%       x 每一列是一个数据点
+%   输出：
+%       y = 0.5*x'*A*x+B*x+C 二次函数，因为x包含多个数据点，所以使用下面的计算公式
+%   
     [~,N] = size(x);
     y = 0.5*sum((x'*A).*x',2)' + B*x + repmat(C,1,N);
 end
