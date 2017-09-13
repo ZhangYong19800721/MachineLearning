622b022f65c01760995307d3f0fefa27d3c26d29
ZhangYong19800721 <24452861@qq.com>
2017-09-13 09:12:14 +0800

    update

--- +learn/+optimal/AR.m
+++ +learn/+optimal/AR.m
@@ -8,23 +8,30 @@ function [a,b]=AR(F,x0,h0)
 %   a 搜索区间左端点
 %   b 搜索区间右端点
 
-    x1 = x0;      F1 = F.object(x1); 
-    x2 = x0 + h0; F2 = F.object(x2);
+    lamda = 1.0;
+    x1 = x0;              F1 = F.object(x1); 
+    x2 = x0 + lamda * h0; F2 = F.object(x2);
     if F1 > F2
         x = x2; Fx = F2;
         while true
-            h0 = 2 * h0; % 扩大步长
-            x1 = x2;
-            x2 = x1 + h0; 
+            lamda = 2 * lamda; % 扩大步长
+            x1 = x2; 
+            x2 = x1 + lamda * h0; 
             F2 = F.object(x2);
             if F2 > Fx
                 break;
             end
         end
     else
-        h0 = 0.5 * h0;
+        x = x1; Fx = F1;
         while true
-            x2 = 
+            lamda = 2 * lamda; % 扩大步长
+            x1 = x2; 
+            x2 = x1 - lamda * h0; 
+            F2 = F.object(x2);
+            if F2 > Fx
+                break;
+            end
         end
     end
     
