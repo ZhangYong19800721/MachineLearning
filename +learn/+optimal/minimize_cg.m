40ef23e75426a4c77ccbddf64297d4f9abad6a07
ZhangYong19800721 <24452861@qq.com>
2017-09-15 15:43:12 +0800

    u

--- +learn/+optimal/minimize_cg.m
+++ +learn/+optimal/minimize_cg.m
@@ -76,7 +76,7 @@ function [x1,y1] = minimize_cg(F,x0,parameters)
             [a,b] = learn.optimal.ARR(Fs,0,1,parameters.parabola); % 确定搜索区间
             [y2,lamda] = learn.optimal.parabola(Fs,a,b,parameters.parabola); x2 = x1 + lamda * d1;
         elseif strcmp(parameters.option,'armijo') % armijo准则进行一维非精确搜索
-            [~,y2,x2] = learn.optimal.armijo(F,x1,g1,d1,parameters.armijo);
+            [y2,x2] = learn.optimal.armijo(F,x1,g1,d1,parameters.armijo);
         end
         
         c1 = mod(it,parameters.reset) == 0; % 到达重置点
