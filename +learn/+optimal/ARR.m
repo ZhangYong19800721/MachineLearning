bc969ca607d1746e734b9b567c588c7b1c05389b
ZhangYong19800721 <24452861@qq.com>
2017-09-15 13:42:58 +0800

    u

--- +learn/+optimal/ARR.m
+++ +learn/+optimal/ARR.m
@@ -1,4 +1,4 @@
-function [a,b]=ARR(F,x0,h0,epsilon)
+function [a,b]=ARR(F,x0,h0,parameters)
 % ARR 改进的进退法确定射线搜索区间
 % 输入：
 %   F 函数对象，调用F.object(x)计算目标函数
@@ -8,6 +8,18 @@ function [a,b]=ARR(F,x0,h0,epsilon)
 %   a 搜索区间左端点
 %   b 搜索区间右端点
 
+    %% 参数设置
+    if nargin <= 3 % 没有给出参数的情况
+        parameters = [];
+        % disp('调用gold函数时没有给出参数集，将使用默认参数');
+    end
+    
+    if ~isfield(parameters,'epsilon') % 给出参数但是没有给出epsilon的情况
+        parameters.epsilon = 1e-6; 
+        % disp(sprintf('调用ARR函数时参数集中没有epsilon参数，将使用默认值%f',parameters.epsilon));
+    end
+
+    %%
     F0 = F.object(x0); 
     x1 = x0 + h0; F1 = F.object(x1);
     
@@ -22,7 +34,7 @@ function [a,b]=ARR(F,x0,h0,epsilon)
         a = x0;
         while F0 <= F1
             h0 = 0.5 * h0; % 缩小步长
-            if abs(h0) < epsilon
+            if abs(h0) < parameters.epsilon
                 break;
             end
             b  = x1;
