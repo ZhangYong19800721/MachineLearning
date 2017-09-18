fd7a9e7db6a00ee54386cf4387d37fa413761a77
root <root>
2017-09-18 10:37:23 +0800

    u

--- test.m
+++ test.m
@@ -1,19 +1,19 @@
 clear all
 close all
 
-load('images.mat');
-load('sae_pretrain.mat');
+load('images.mat'); points = points(1:(32*32),:);
+load('sae_pretrain_1024x512x64.mat');
 
 [D,N] = size(points);
 points = double(points) / 255;
 code = sae.encode(points,'fix');
 
-query_id = 3;
+query_id = 5;
 query_code = code(:,query_id);
 query_code = repmat(query_code,1,N);
 match_idx = sum(xor(query_code,code)) <= 5;
 
-dir = 'D:\视频基因\02-TrainData\imagebasev3\';
+dir = 'D:\imagebasev3\视频基因训练数据\imagebasev3\';
 seq = 1:N;
 match = seq(match_idx);
 for n = match
