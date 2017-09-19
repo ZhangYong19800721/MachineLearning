a61b57abc7f1c5668f94a639e43cdb2a8b11be20
ZhangYong19800721 <24452861@qq.com>
2017-09-19 19:28:45 +0800

    u

--- +learn/+tools/whiten.m
+++ +learn/+tools/whiten.m
@@ -1,13 +1,32 @@
-function [W,D,A] = whiten(X)
-%WHITEN ZCA Whitening Transform
-%   参考“Learning Multiple Layers of Features from Tiny Images”的Appendix A
+classdef whiten
+    %WHITEN ZCA Whitening Transform
+    %   参考“Learning Multiple Layers of Features from Tiny Images”的Appendix A
+    properties
+        W;  % 白化矩阵
+        D;  % 解白化矩阵
+        A;  % 均值向量
+    end
 
-    [~,N] = size(X);
-    A = mean(X,2);
-    X = double(X) - repmat(A,1,N);
-    [U,K,V] = svd(X*X');
-    G = diag(K); G(G==0) = inf; G = 1./G; G = diag(G);
-    W =     sqrt(N-1) * V * sqrt(G) * U'; 
-    D = 1 / sqrt(N-1) * U * sqrt(K) * V';
+    methods
+        function obj = compute(obj,X)
+            [~,N] = size(X);
+            obj.A = mean(X,2);
+            X = double(X) - repmat(obj.A,1,N);
+            [U,K,V] = svd(X*X');
+            G = diag(K); G(G==0) = inf; G = 1./G; G = diag(G);
+            obj.W =     sqrt(N-1) * V * sqrt(G) * U';
+            obj.D = 1 / sqrt(N-1) * U * sqrt(K) * V';
+        end
+        
+        function Y = white(X)
+            Y = X - repmat(obj.A,1,size(X,2));
+            Y = obj.W * Y;
+        end
+        
+        function X = dewhite(Y)
+            X = obj.D * Y;
+            X = X + repmat(obj.A,1,size(X,2));
+        end
+    end
 end
 
