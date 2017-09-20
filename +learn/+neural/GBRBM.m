2ef20d2fc7d2ec10fd7a6538116685ed7a630b5e
ZhangYong19800721 <24452861@qq.com>
2017-09-20 14:28:36 +0800

    u

--- +learn/+neural/GBRBM.m
+++ +learn/+neural/GBRBM.m
@@ -5,162 +5,180 @@ classdef GBRBM
     properties
         num_hidden;  % 隐藏神经元的个数
         num_visual;  % 可见神经元的个数
-        weight;      % 权值矩阵(num_hidden * num_visual)
+        weight_v2h;  % 权值矩阵(num_hidden * num_visual)
+        weight_h2v;  % 权值矩阵(num_hidden * num_visual)
         hidden_bias; % 隐藏神经元的偏置
         visual_bias; % 可见神经元的偏置
+        visual_sstd; % 可见神经元标准差
+        points; % 训练数据
     end
     
     methods
         function obj = GBRBM(num_visual,num_hidden) % 构造函数
-            obj.num_hidden = num_hidden;
-            obj.num_visual = num_visual;
-            obj.weight = zeros(obj.num_hidden,obj.num_visual);
+            obj.num_hidden  = num_hidden;
+            obj.num_visual  = num_visual;
+            obj.weight_v2h  = zeros(obj.num_hidden,obj.num_visual);
+            obj.weight_h2v  = [];
             obj.hidden_bias = zeros(obj.num_hidden,1);
             obj.visual_bias = zeros(obj.num_visual,1);
         end
     end
     
     methods
-
-        function obj = initialize(obj,minibatchs) 
-            %initialize 基于训练数据（由多个minibatch组成的训练数据集合）初始化权值矩阵，隐神经元偏置和显神经元偏置
-            %           minibatchs 是一个[D,S,M]维的数组。
-           
-            [D,S,M] = size(minibatchs);
-            minibatchs = reshape(minibatchs,D,[]);
-            obj = obj.initialize_weight(minibatchs);
+        %% 计算梯度
+        function g = gradient(obj,x,i)
+            %% 嵌入参数
+            H = obj.num_hidden; V = obj.num_visual; W = H*V;
+            obj.weight_v2h (:) = x(     1:W );
+            obj.hidden_bias(:) = x(W+  (1:H));
+            obj.visual_bias(:) = x(W+H+(1:V));
+            
+            %%
+            [D,S,M] = size(obj.points);
+            i = 1 + mod(i,M);
+            minibatch = obj.points(:,:,i);
+            N = size(minibatch,2); % 训练样本的个数
+            h_bias = repmat(obj.hidden_bias,1,N);
+            v_bias = repmat(obj.visual_bias,1,N);
+            v_sstd = repmat(obj.visual_sstd,1,N);
+            
+            %%
+            h_field_0 = learn.tools.sigmoid(obj.weight_v2h * (minibatch ./ v_sstd) + h_bias);
+            h_state_0 = learn.tools.sample(h_field_0);
+            v_field_1 = v_sstd .* (obj.weight_v2h'* h_state_0) + v_bias;
+            v_state_1 = v_field_1 + v_sstd .* randn(size(v_field_1));
+            h_field_1 = learn.tools.sigmoid(obj.weight_v2h * (v_state_1 ./ v_sstd) + h_bias);
+            gw = (h_field_0 * (minibatch ./ v_sstd)' - h_field_1 * (v_state_1 ./ v_sstd)') / N;
+            gh = (h_field_0 - h_field_1) * ones(N,1) / N;
+            gv = ((minibatch - v_state_1) ./ v_sstd.^2) * ones(N,1) / N;
+            
+            g1 = ((minibatch - v_bias).^2 ./ v_sstd.^3) * ones(N,1) / N;
+            g2 = (minibatch ./ v_sgma.^2) .* (obj.weight_v2h' * h_field_0) * ones(N,1) / N;
+            g3 = ((v_state_1 - v_bias).^2 ./ v_sgma.^3) * ones(N,1) / N;
+            g4 = (v_state_1 ./ v_sgma.^2) .* (obj.weight_v2h' * h_field_1) * ones(N,1) / N;
+            gd = (g1 - g2) - (g3 - g4);
+            
+            weight_cost = 1e-4; cw = weight_cost * obj.weight_v2h;
+            g = -[gw(:)-cw(:);gh(:);gv(:);gd(:)];
         end
-
-        function obj = pretrain(obj,minibatchs,parameters) 
+        
+        %% 计算重建误差
+        function y = object(obj,x,i)
+            %% 嵌入参数
+            H = obj.num_hidden; V = obj.num_visual; W = H*V;
+            obj.weight_v2h(:)  = x(     1:W );
+            obj.hidden_bias(:) = x(W+  (1:H));
+            obj.visual_bias(:) = x(W+H+(1:V));
+            
+            %%
+            [D,S,M] = size(obj.points);
+            i = 1 + mod(i,M);
+            minibatch = obj.points(:,:,i);
+            N = size(minibatch,2); % 训练样本的个数
+            h_bias = repmat(obj.hidden_bias,1,N);
+            v_bias = repmat(obj.visual_bias,1,N);
+            
+            %% 计算重建误差
+            h_field_0 = learn.tools.sigmoid(obj.weight_v2h * minibatch + h_bias);
+            h_state_0 = learn.tools.sample(h_field_0);
+            v_field_1 = obj.weight_v2h'* h_state_0 + v_bias;
+            y =  sum(sum((v_field_1 - minibatch).^2)) / N; %计算在整个minibatch上的平均重建误差
+        end
+        
+        function obj = construct(obj,weight_v2h,weight_h2v,visual_bias,hidden_bias)
+            %construct 使用权值、隐神经元偏置、显神经元偏置直接构建RBM
+            [obj.num_hidden, obj.num_visual] = size(weight_v2h);
+            obj.weight_v2h = weight_v2h;
+            obj.weight_h2v = weight_h2v;
+            obj.hidden_bias = hidden_bias;
+            obj.visual_bias = visual_bias;
+        end
+        
+        function obj = initialize(obj,points)
+            %initialize 基于训练数据（由多个minibatch组成的训练数据集合）初始化权值矩阵，隐神经元偏置和显神经元偏置
+            %           points 是一个[D,S,M]维的数组。
+            
+            [D,S,M] = size(points);
+            points = reshape(points,D,[]);
+            obj = obj.initialize_weight(points);
+        end
+        
+        function obj = pretrain(obj,points,parameters)
             %pretrain 对权值进行预训练
             % 使用CD1快速算法对权值进行预训练
             
-            [D,S,M] = size(minibatchs); % 得到minibatch的个数
-            ob_window_size = M;     % 设定观察窗口的大小为
-            ob_var_num = 1;                     % 设定观察变量的个数
-            ob = learn.tools.Observer('重建误差',ob_var_num,ob_window_size); %初始化观察者，观察重建误差
+            obj.points = points; % 绑定训练数据
+            x0 = [obj.weight_v2h(:); obj.hidden_bias(:); obj.visual_bias(:)]; % 设定迭代起始点
+            x = learn.optimal.minimize_sgd(obj,x0,parameters); % 随机梯度下降
             
-            % 初始化velocity变量
-            v_weight = zeros(size(obj.weight));
-            v_h_bias = zeros(size(obj.hidden_bias));
-            v_v_bias = zeros(size(obj.visual_bias));
-            v_v_sgma = zeros(size(obj.visual_sgma));
+            %% 嵌入参数
+            H = obj.num_hidden; V = obj.num_visual; W = H*V;
+            obj.weight_v2h(:)  = x(     1:W );
+            obj.hidden_bias(:) = x(W+  (1:H));
+            obj.visual_bias(:) = x(W+H+(1:V));
             
-            % 初始化动量倍率为0.5
-            momentum = 0.5;
-            
-            r_error_list = zeros(1,ob_window_size);
-            for idx = 1:M  % 初始化重建误差列表的移动平均值
-                minibatch = minibatchs(:,:,idx);
-                [~, ~, ~, ~, r_error] = obj.CD1(minibatch);
-                r_error_list(idx) = r_error;
-            end
-            r_error_ave_old = mean(r_error_list);
-            ob = ob.initialize(r_error_ave_old);
-            
-            learn_rate_min = min(parameters.learn_rate);
-            learn_rate     = max(parameters.learn_rate);  %初始化学习速度
-            
-            for it = 0:parameters.max_it
-                minibatch_idx = mod(it,M)+1;  % 取一个minibatch
-                minibatch = minibatchs(:,:,minibatch_idx);
-                
-                [d_weight, d_h_bias, d_v_bias, d_v_sgma, r_error] = obj.CD1(minibatch);
-                r_error_list(minibatch_idx) = r_error;
-                r_error_ave_new = mean(r_error_list);
-                
-                if minibatch_idx == M % 当所有的minibatch被轮讯了一遍的时候（到达观察窗口最右边的时候）
-                    if r_error_ave_new > r_error_ave_old
-                        learn_rate = learn_rate / 5;
-                        if learn_rate < learn_rate_min
-                            break;
-                        end
-                    end
-                    r_error_ave_old = r_error_ave_new;
-                end
-                
-                description = strcat('重建误差:',num2str(r_error_ave_new));
-                description = strcat(description,strcat('迭代次数:',num2str(it)));
-                description = strcat(description,strcat('学习速度:',num2str(learn_rate)));
-                disp(description);
-                ob = ob.showit(r_error_ave_new,description);
-                
-                momentum = min([momentum * 1.01,0.9]); % 动量倍率最大为0.9，初始值为0.5，大约迭代60步之后动量倍率达到0.9。
-                v_weight = momentum * v_weight + learn_rate * (d_weight - parameters.weight_cost * obj.weight);
-                v_h_bias = momentum * v_h_bias + learn_rate * d_h_bias;
-                v_v_bias = momentum * v_v_bias + learn_rate * d_v_bias; 
-                v_v_sgma = momentum * v_v_sgma + learn_rate * parameters.learn_sgma * d_v_sgma;
-                
-                obj.weight      = obj.weight      + v_weight;
-                obj.hidden_bias = obj.hidden_bias + v_h_bias;
-                obj.visual_bias = obj.visual_bias + v_v_bias;
-                obj.visual_sgma = obj.visual_sgma + v_v_sgma;
-                obj.visual_sgma(obj.visual_sgma <= 5e-3) = 5e-3;
+            obj.points = []; % 解除绑定训练数据
+        end
+        
+        function h_state = posterior_sample(obj,v_state)
+            % posterior_sample 计算后验概率采样
+            % 在给定显层神经元取值的情况下，对隐神经元进行抽样
+            h_state = learn.tools.sample(obj.posterior(v_state));
+        end
+        
+        function h_field = posterior(obj,v_state)
+            %POSTERIOR 计算后验概率
+            % 在给定显层神经元取值的情况下，计算隐神经元的激活概率
+            h_field = learn.tools.sigmoid(obj.foreward(v_state));
+        end
+        
+        function v_state = likelihood_sample(obj,h_state)
+            % likelihood_sample 计算似然概率采样
+            % 在给定隐层神经元取值的情况下，对显神经元进行抽样
+            v_state = obj.likelihood(h_state);
+            v_state = v_state + randn(size(v_state));
+        end
+        
+        function v_field = likelihood(obj,h_state)
+            % likelihood 计算似然概率
+            % 在给定隐层神经元取值的情况下，计算显神经元的激活概率
+            v_field = obj.backward(h_state);
+        end
+        
+        function y = foreward(obj,x)
+            y = obj.weight_v2h * x + repmat(obj.hidden_bias,1,size(x,2));
+        end
+        
+        function x = backward(obj,y)
+            if isempty(obj.weight_h2v)
+                x = obj.weight_v2h'* y + repmat(obj.visual_bias,1,size(y,2));
+            else
+                x = obj.weight_h2v'* y + repmat(obj.visual_bias,1,size(y,2));
             end
         end
         
-        function y = reconstruct(obj,x)
-            N = size(x,2); % 样本的个数
-            h_bias = repmat(obj.hidden_bias,1,N);
-            v_bias = repmat(obj.visual_bias,1,N);
-            v_sgma = repmat(obj.visual_sgma,1,N);
-            
-            h_field_0 = learn.sigmoid(obj.weight * (x ./ v_sgma) + h_bias);
-            h_state_0 = learn.sample(h_field_0);
-            y = v_sgma .* (obj.weight'* h_state_0) + v_bias;
+        function y = rebuild(obj,x)
+            z = obj.posterior_sample(x);
+            y = obj.likelihood(z);
+        end
+        
+        function obj = weightsync(obj)
+            obj.weight_h2v = obj.weight_v2h;
         end
         
     end
     
     methods (Access = private)
-        function [d_weight,d_h_bias,d_v_bias,d_v_sgma,r_error] = CD1(obj, minibatch)
-            % 使用Contrastive Divergence 1 (CD1)方法对约束玻尔兹曼RBM机进行快速训练
-            % 输入：
-            %   minibatch，一组训练数据，一列代表一个训练样本，列数表示训练样本的个数
-            % 输出
-            %   d_weight, 权值矩阵的迭代差值.
-            %   d_h_bias, 隐藏神经元偏置值的迭代差值.
-            %   d_v_bias, 可见神经元偏置值的迭代差值.
-            %   r_error,  重建误差值
-            
-            N = size(minibatch,2); % 训练样本的个数
-            h_bias = repmat(obj.hidden_bias,1,N);
-            v_bias = repmat(obj.visual_bias,1,N);
-            v_sgma = repmat(obj.visual_sgma,1,N);
-            
-            h_field_0 = learn.tools.sigmoid(obj.weight * (minibatch ./ v_sgma) + h_bias);
-            h_state_0 = learn.tools.sample(h_field_0);
-            v_field_1 = v_sgma .* (obj.weight'* h_state_0) + v_bias;
-            v_state_1 = v_field_1 + v_sgma .* randn(size(v_field_1));
-            h_field_1 = learn.tools.sigmoid(obj.weight * (v_state_1 ./ v_sgma) + h_bias);
-            h_state_1 = learn.tools.sample(h_field_1);
-            
-            r_error =  sum(sum((v_field_1 - minibatch).^2)) / N; %计算在整个minibatch上的平均重建误差
-            
-            d_weight = (h_field_0 * (minibatch ./ v_sgma)' - h_field_1 * (v_state_1 ./ v_sgma)') / N;
-            d_h_bias = (h_field_0 - h_field_1) * ones(N,1) / N;
-            d_v_bias = ((minibatch - v_state_1) ./ (v_sgma.^2)) * ones(N,1) / N;
-            
-            d_v_sgma1 = (((minibatch - v_bias).^2) ./ (v_sgma.^3)) * ones(N,1) / N;
-            d_v_sgma2 = (minibatch ./ (v_sgma.^2)) .* (obj.weight' * h_field_0) * ones(N,1) / N;
-            d_v_sgma3 = (((v_state_1 - v_bias).^2) ./ (v_sgma.^3)) * ones(N,1) / N;
-            d_v_sgma4 = (v_state_1 ./ (v_sgma.^2)) .* (obj.weight' * h_field_1) * ones(N,1) / N;
-            d_v_sgma = (d_v_sgma1 - d_v_sgma2) - (d_v_sgma3 - d_v_sgma4);
-        end
-        
-        function obj = initialize_weight(obj,train_data)
+        function obj = initialize_weight(obj,points)
             %INTIALIZE 初始化权值矩阵为0附近的小随机数，初始化显层神经元的偏置为先验概率，初始化隐层神经元的偏置为0.
-            obj.weight = 0.01 * randn(size(obj.weight));
-            obj.visual_bias = mean(train_data,2);
+            obj.weight_v2h  = 0.01 * randn(size(obj.weight_v2h));
+            obj.visual_bias = mean(points,2);
             obj.hidden_bias = zeros(size(obj.hidden_bias));
-            obj.visual_sgma = std(train_data,0,2);
-            obj.visual_sgma(obj.visual_sgma <= 5e-3) = 5e-3;
-            obj.visual_sgma(obj.visual_sgma >= 1   ) = 1;
         end
     end
     
     methods(Static)
-         function [] = unit_test()
+        function [] = unit_test()
             clear all;
             close all;
             rng(1);
@@ -174,25 +192,24 @@ classdef GBRBM
             
             ave_data = mean(data,2);
             std_data = std(data,0,2);
-            
+            data = data(std_data>0.01,:);
+            D = size(data,1);
             data = reshape(data,D,S,M);
             
             gbrbm = learn.neural.GBRBM(D,500);
             gbrbm = gbrbm.initialize(data);
             
-            parameters.learn_rate = [1e-8,1e-4];
-            parameters.learn_sgma = 1e-2;
-            parameters.weight_cost = 1e-4;
-            parameters.max_it = 1e6;
-
+            parameters.learn_rate = 1e-4;
+            parameters.max_it = M*10;
+            
             gbrbm = gbrbm.pretrain(data,parameters);
-            save('gbrbm.mat','gbrbm');
-         
+            
             data = reshape(data,D,[]);
-            recon_data = gbrbm.reconstruct(data);
-            recon_data = recon_data / 10;
-            recon_mnist = R * recon_data + repmat(A,1,size(recon_data,2));
-            e = sum(sum((recon_mnist - mnist).^2)) / (S*M);
+            rebuild_data = gbrbm.rebuild(data);
+            rebuild_data = [rebuild_data; zeros(74,S*M)];
+            rebuild_mnist = whiten.dewhite(rebuild_data);
+            e = sum(sum((rebuild_mnist - mnist).^2)) / (S*M);
+            disp(sprintf('重建误差:%f',e));
         end
     end
     
