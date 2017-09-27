356837cc2a1adde814737e1a6fba29146d682d75
ZhangYong19800721 <24452861@qq.com>
2017-09-27 09:02:13 +0800

    u

--- test.m
+++ test.m
@@ -1,23 +1,25 @@
 clear all
 close all
 
-load('images.mat'); points = points(1:(32*32),:);
-load('sae_pretrain_1024x512x64.mat');
+load('images.mat'); % points = points(1:(32*32),:);
+load('sae_pretrained.mat');
 
 [D,N] = size(points);
 points = double(points) / 255;
 code = sae.encode(points,'fix');
 
-query_id = 5;
+query_id = 4000;
 query_code = code(:,query_id);
 query_code = repmat(query_code,1,N);
-match_idx = sum(xor(query_code,code)) <= 5;
+match_idx = sum(xor(query_code,code)) <= 0;
 
-dir = 'D:\imagebasev3\视频基因训练数据\imagebasev3\';
+dir = 'D:\imagebasev3\';
 seq = 1:N;
 match = seq(match_idx);
+k = 0;
 for n = match
+    k = k + 1
     file = strcat(strcat(dir,sprintf('%09d',n-1)),'.jpg');
     image = imread(file);
-    imshow(image);
+    imshow(imresize(image,0.5));
 end
